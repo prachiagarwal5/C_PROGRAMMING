a59888d444d00be69afab34d4d98a7b0346d5e2f
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-03-16 10:17:36 -0700

    Create table.c

---
+++ table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+int i=1;
+printf("enter the number");
+scanf ("%d",&n);
+while(i<=10){
+    printf("%d*%d=%d\n",n,i,n*i);
+    i++;
+}
+
+    return 0;
+}
