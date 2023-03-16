11a1aa293ed713aed9072aca4e3af3ad28bab6b9
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-03-16 10:12:40 -0700

    Create sum of n natural numbers .c

---
+++ sum of n natural numbers .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+int i=0;
+int s=0;
+printf("enter the number");
+scanf ("%d",&n);
+while(i<=n){
+
+   s+=i;
+    i++;
+}
+printf("%d\n",s);
+    return 0;
+}
