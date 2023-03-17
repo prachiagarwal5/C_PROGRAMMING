339e4f8b026ca7cb43c5ed0ed30a72d99ac5773e
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-03-17 09:34:42 +0530

    Create last and first digit.c

---
+++ last and first digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int c;
+
+    
+    printf("enter the number");
+    scanf("%d",&a);
+ c=a%10;
+    while(a>10){
+ a=a/10;
+     }
+   
+    
+      printf("%d\n",c);
+       printf("%d",a);
+ }
