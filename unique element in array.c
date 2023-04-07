451903e5bcfd20e4242c825ae8816dca0feefa08
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-04-07 08:47:29 +0530

    Create unique element in array.c

---
+++ unique element in array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    int a[n],i,max,b[100]={0};
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    max=a[0];
+    for(i=0;i<n;i++){
+        if(a[i]>max)
+        max=a[i];
+    }
+    max++;
+    for(i=0;i<n;i++)
+        b[a[i]]++;
+    
+    for(i=0;i<max;i++){
+        if (b[i]==1)
+        printf("%d",i);
+    }
+ return 0;   
+}
