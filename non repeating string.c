1f5d71bba7b335cb0ca15e5c972c8b162df2ec0e
prachiagarwal5 <113891659+prachiagarwal5@users.noreply.github.com>
2023-05-11 11:01:28 -0700

    Create non repeating string.c

---
+++ non repeating string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+char a[100];
+int b[26]={0};
+printf("enter the string");
+scanf("%s",a);
+int i;
+for(i=0;a[i];i++){
+    b[a[i]-97]++;
+}
+for(i=0;i<26;i++){
+    if(b[i]==1)
+    printf("%c",i+97);
+}
+
+    return 0;
+}
